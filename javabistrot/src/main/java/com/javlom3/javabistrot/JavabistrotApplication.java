1f811347e3318f6d6aa6e26657cf88ef1cc4e63c
Devencode <venelin.parvanov@outlook.com>
2026-02-16 12:03:13 +0100

    Aggiunto file spring initializer

---
+++ javabistrot/src/main/java/com/javlom3/javabistrot/JavabistrotApplication.java
@@ -0,0 +1,13 @@
+package com.javlom3.javabistrot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavabistrotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavabistrotApplication.class, args);
+	}
+
+}
---
+++ javabistrot/src/test/java/com/javlom3/javabistrot/JavabistrotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javlom3.javabistrot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavabistrotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
