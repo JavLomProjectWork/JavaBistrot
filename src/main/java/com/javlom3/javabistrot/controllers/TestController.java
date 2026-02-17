f937720cf024e1e9ed3f901e4016c32a51d7eabd
Mauro Fratoni <mauro.fratoni@outlook.com>
2026-02-17 18:04:40 +0100

    added testcontroller and test template

---
+++ src/main/java/com/javlom3/javabistrot/controllers/TestController.java
@@ -0,0 +1,13 @@
+package com.javlom3.javabistrot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/test")
+    public String testPage() {
+        return "test";
+    }
+}
